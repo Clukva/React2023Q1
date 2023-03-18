59386a9ebe3e58becb83ca59ed5da1abcd36e928
Clukva <yura4-1-1@mail.ru>
2023-03-18 22:49:47 +0300

    feat: add header which shows the current page

--- my-react-app/src/components/Header.tsx
+++ my-react-app/src/components/Header.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const addHeader: FC = () => {
   return (
     <header>
-      <Link className="header-link" to="/">
+      <NavLink className="header-link" to="/">
         Home
-      </Link>
-      <Link className="header-link" to="/about">
+      </NavLink>
+      <NavLink className="header-link" to="/about">
         About Us
-      </Link>
+      </NavLink>
     </header>
   );
 };
