78f914fea98086338ce307a5a4f9abd6ac07cc73
Clukva <yura4-1-1@mail.ru>
2023-03-19 08:37:40 +0300

    feat: add logic to input element

--- my-react-app/src/components/MyCharacter.tsx
+++ my-react-app/src/components/MyCharacter.tsx
@@ -22,6 +22,7 @@ interface IState {
   error: null | { message: string };
   isLoaded: boolean;
   items: Array<IItems>;
+  inputValue: string;
 }
 
 export default class MyCharacter extends React.Component<IProps, IState> {
@@ -31,10 +32,15 @@ export default class MyCharacter extends React.Component<IProps, IState> {
       error: null,
       isLoaded: false,
       items: [],
+      inputValue: '',
     };
+    this.addInputChange = this.addInputChange.bind(this);
   }
 
   componentDidMount() {
+    const inputValue = localStorage.getItem('input') || '';
+    this.setState({ inputValue });
+
     fetch('https://rickandmortyapi.com/api/character/?page=8')
       .then((res) => res.json())
       .then(
@@ -53,8 +59,17 @@ export default class MyCharacter extends React.Component<IProps, IState> {
       );
   }
 
+  componentWillUnmount(): void {
+    const { inputValue } = this.state;
+    localStorage.setItem('input', inputValue);
+  }
+
+  addInputChange(ev: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ inputValue: ev.target.value });
+  }
+
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, items, inputValue } = this.state;
 
     if (error) {
       return <div>Ошибка: {error.message}</div>;
@@ -67,26 +82,43 @@ export default class MyCharacter extends React.Component<IProps, IState> {
       return <img className="card-image" src={url} alt="character" />;
     }
     return (
-      <div>
-        {items?.map((item) => (
-          <React.Fragment key={item.id}>
-            <section className="main-card">
-              <section>{renderImg(item.image)}</section>
-              <section className="card-description">
-                Name <b>{item.name}</b>
-                <br />
-                Gender <b>{item.gender}</b>
-                <br />
-                Species <b>{item.species}</b>
-                <br />
-                Type <b>{item.type}</b>
-                <br />
-                Status <b>{item.status}</b>
-              </section>
-            </section>
-          </React.Fragment>
-        ))}
-      </div>
+      <>
+        <div className="main-page">
+          <div className="main-page--form">
+            <form action="">
+              <input
+                className="main-page--input"
+                type="text"
+                placeholder="Search"
+                value={inputValue}
+                onChange={this.addInputChange}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="main-cards">
+          <div>
+            {items?.map((item) => (
+              <React.Fragment key={item.id}>
+                <section className="main-card">
+                  <section>{renderImg(item.image)}</section>
+                  <section className="card-description">
+                    Name <b>{item.name}</b>
+                    <br />
+                    Gender <b>{item.gender}</b>
+                    <br />
+                    Species <b>{item.species}</b>
+                    <br />
+                    Type <b>{item.type}</b>
+                    <br />
+                    Status <b>{item.status}</b>
+                  </section>
+                </section>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
--- my-react-app/src/pages/Mainpage.tsx
+++ my-react-app/src/pages/Mainpage.tsx
@@ -2,20 +2,7 @@ import React, { FC } from 'react';
 import CardComponent from '../components/MyCharacter';
 
 const addMainPage: FC = () => {
-  return (
-    <>
-      <div className="main-page">
-        <div className="main-page--form">
-          <form action="">
-            <input className="main-page--input" type="text" placeholder="Search" />
-          </form>
-        </div>
-      </div>
-      <div className="main-cards">
-        <CardComponent />
-      </div>
-    </>
-  );
+  return <CardComponent />;
 };
 
 export default addMainPage;
