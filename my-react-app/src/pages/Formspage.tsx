436c63afd2b326362d3a131dc00d0c5614a26113
Clukva <yura4-1-1@mail.ru>
2023-03-21 22:04:32 +0300

    feat: add inputs text, date, checkbox and select

--- my-react-app/src/pages/Formspage.tsx
+++ my-react-app/src/pages/Formspage.tsx
@@ -2,9 +2,39 @@ import React, { FC } from 'react';
 
 const addFormsPage: FC = () => {
   return (
-    <div className="main-error">
-      <h1>Forms</h1>
-    </div>
+    <form className="form-conteiner">
+      <label htmlFor="name">
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <label htmlFor="surname">
+        Surname:
+        <input type="text" name="surname" />
+      </label>
+      <label htmlFor="birthday">
+        Birthday:
+        <input type="date" name="birthday" />
+      </label>
+      <label htmlFor="country">
+        Country:
+        <select name="country">
+          <option value="Belarus">Belarus</option>
+          <option value="Litunia">Litunia</option>
+          <option value="Latvia">Latvia</option>
+          <option value="Poland">Poland</option>
+          <option value="Ukraine">Ukraine</option>
+          <option value="Russia">Russia</option>
+          <option value="Another country">Another country</option>
+        </select>
+      </label>
+      <label htmlFor="birthpersonalData">
+        <input type="checkbox" name="birthpersonalData" />I consent to my personal data
+        <label htmlFor="newsletter">
+          <input type="checkbox" name="newsletter" />
+          Subscribe to the newsletter
+        </label>
+      </label>
+    </form>
   );
 };
 
