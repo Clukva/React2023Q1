90c0afb7776950c2066fc81a8ef580ba33850b12
Clukva <yura4-1-1@mail.ru>
2023-04-28 11:37:11 +0300

    feat: add cypress to project

---
+++ my-react-app/cypress/e2e/home_page.cy.ts
@@ -0,0 +1,6 @@
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3001'); // change URL to match your dev URL
+    cy.visit('/about');
+  });
+});
